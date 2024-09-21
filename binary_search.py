08abb8347e296f2ecb829b45f740ed724b6ed22b
Arjun <Silwal>
2024-09-21 10:02:16 -0500

    renamed from hello and linear search added

--- binary_search.py
+++ binary_search.py
@@ -9,7 +9,7 @@ import math
 
 
 #linear Search
-def locate_card_position(cards, query):
+def locate_number(cards, query):
     position = 0
     while position < len(cards):
         if cards[position] == query:
@@ -19,18 +19,32 @@ def locate_card_position(cards, query):
 
 
 tests = []
-test1 = {
-    "input": {
-        "cards": [12, 8, 7, 4, 3, 2],
+
+tests.append({
+    "input":{
+        "cards":[12,9,7,5,4],
         "query": 7
     },
     "output": 2
-}
+})
+tests.append({
+    "input":{
+        "cards": [2],
+        "query": 2
+    },
+    "output": 0
+})
+tests.append({
+    "input":{
+        "cards": [10,6,8,7],
+        "query": 5
+    },
+    "output": -1
+})
+print(tests)
 
-tests.append(test1)
-result = locate_card_position(**test1["input"])
-print(result)
-if result == test1["output"]:
-    print("result found")
-else:
-    print("not found")
+for i in range(len(tests)):
+    if locate_number(**tests[i]["input"]) == tests[i]["output"]:
+        print("Passed")
+    else:
+        print("Failed")
